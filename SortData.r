6ce0b290301cc95fc9205b17b686e518bf719b8f
CD11 <cheryl.dunn40@gmail.com>
2017-03-25 18:23:36 -0400

    updates to scheme type in sortData

--- SortData.r
+++ SortData.r
@@ -5,56 +5,53 @@ img_pts <- read.csv(file="Imagept28_log.csv", head=TRUE,sep=",")
 
 
 #####################    TEXT28 Scheme          #########################################
-users <- unique(text28$user)
-print(users)
-df <- data.frame(text28$user, text28$scheme, text28$mode, text28$event, text28$time)
-print(df)
+users_text <- unique(text28$user)
+df_text <- data.frame(text28$user, text28$scheme, text28$mode, text28$event, text28$time)
 
-c = 0 
-#testing <- data.frame("user", " scheme", "numLogin", "numSucess", "numFail")
-for(user in users){
-  logins = length(subset(df$mode, text28$user == user &text28$mode == "login"))
-  success = length(subset(df$text28.event, text28$user == user &text28$mode == "login" & text28$event == "success"))
-  fail =  length(subset(df$text28.event, text28$user == user &text28$mode == "login" & text28$event == "failure"))
-  scheme = subset(df$text28.scheme, text28$user == user) 
-  temp <- (data.frame(user, scheme, logins, success, fail))
-
+info <- data.frame(user = character(0), scheme = character(0), numLogin = numeric(), numSucess = numeric(), numFail = numeric())
+for(user in users_text){
+  logins = length(subset(df_text$text28.mode, text28$user == user & text28$mode == "login"))
+  success = length(subset(df_text$text28.event, text28$user == user &text28$mode == "login" & text28$event == "success"))
+  fail =  length(subset(df_text$text28.event, text28$user == user &text28$mode == "login" & text28$event == "failure"))
+  scheme = "text28"
+   temp <- (data.frame(user, scheme, logins, success, fail))
+  info <- rbind(info, temp)
 }
-write.csv(temp, file = "TextScheme.csv")
+print(info)
+write.csv(info, file = "TextScheme.csv")
 
 
 
 #####################    blank_pt Scheme          #########################################
-df <- data.frame(blank_pts$user, blank_pts$scheme, blank_pts$mode, blank_pts$event, blank_pts$time)
-print(df)
-
-c = 0 
-#testing <- data.frame("user", " scheme", "numLogin", "numSucess", "numFail")
-for(user in users){
-  logins = length(subset(df$blank_pts.mode, blank_pts$user == user &blank_pts$mode == "login"))
-  success = length(subset(df$blank_pts.event, blank_pts$user == user &blank_pts$mode == "login" & blank_pts$event == "success"))
-  fail =  length(subset(df$blank_pts.event, blank_pts$user == user &blank_pts$mode == "login" & blank_pts$event == "failure"))
-  scheme = subset(df$blank_pts.scheme, text28$user == user) 
+df_blank <- data.frame(blank_pts$user, blank_pts$scheme, blank_pts$mode, blank_pts$event, blank_pts$time)
+users_blank <- unique(blank_pts$user)
+info <- data.frame(user = character(0), scheme = character(0), numLogin = numeric(), numSucess = numeric(), numFail = numeric())
+for(user in users_blank){
+  logins = length(subset(df_blank$blank_pts.mode, blank_pts$user == user &blank_pts$mode == "login"))
+  success = length(subset(df_blank$blank_pts.event, blank_pts$user == user &blank_pts$mode == "enter" & blank_pts$event == "goodLogin"))
+  fail =  length(subset(df_blank$blank_pts.event, blank_pts$user == user &blank_pts$mode == "enter" & blank_pts$event == "badLogin"))
+  scheme = "blank28"
   temp <- (data.frame(user, scheme, logins, success, fail))
-  
+  info <- rbind(info,temp)
 }
-write.csv(temp, file = "blankScheme.csv")
+print(info)
+write.csv(info, file = "blankScheme.csv")
  
-#####################    img_28 Scheme           #########################################
+#########################    img_28 Scheme           #########################################
 
-users <- unique(img_pts$user)
-df <- data.frame(img_pts$user, img_pts$scheme, img_pts$mode, img_pts$event, img_pts$time)
-print(df)
+users_img <- unique(img_pts$user)
+df_img <- data.frame(img_pts$user, img_pts$scheme, img_pts$mode, img_pts$event, img_pts$time)
 
-c = 0 
-#testing <- data.frame("user", " scheme", "numLogin", "numSucess", "numFail")
-for(user in users){
-  logins = length(subset(df$img_pts.mode, img_pts$user == user & img_pts$mode == "login"))
-  success = length(subset(df$img_pts.event, img_pts$user == user &img_pts$mode == "login" & img_pts$event == "success"))
-  fail =  length(subset(df$img_pts.event, img_pts$user == user &img_pts$mode == "login" & img_pts$event == "failure"))
-  scheme = subset(df$img_pts.scheme, img_pts$user == user) 
-  temp <- data.frame(user, scheme, logins, success, fail)
+info <- data.frame(user = character(0), scheme = character(0), numLogin = numeric(), numSucess = numeric(), numFail = numeric())
+for(user in users_img){
+  logins = length(subset(df_img$img_pts.mode, img_pts$user == user & img_pts$mode == "login"))
+  success = length(subset(df_img$img_pts.event, img_pts$user == user &img_pts$mode == "login" & img_pts$event == "success"))
+  fail =  length(subset(df_img$img_pts.event, img_pts$user == user &img_pts$mode == "login" & img_pts$event == "failure"))
+  #scheme = subset(df$img_pts.scheme, img_pts$user == user) 
+  new <- data.frame(user, scheme, logins, success, fail)
+  info <- rbind(info, new)
 }
-write.csv(temp, file = "ImageScheme.csv")
+print(info)
+write.csv(info, file = "ImageScheme.csv")
 
 
