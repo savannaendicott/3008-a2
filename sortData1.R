60c17d7fbc884aabd686221ca8e451953fb82e0c
CD11 <cheryl.dunn40@gmail.com>
2017-03-26 16:27:26 -0400

    added sortData1
    this sorts data so that it has the time taken for successfull and failed
    logins.
    - this is a better sorting function for analysis than the method used in
    the origianl sortData function

---
+++ sortData1.R
@@ -0,0 +1,67 @@
+setwd("C:/Users/me/Desktop/3008-a2")
+blank_pts <- read.csv(file="Blankpt28_log.csv", head=TRUE,sep=",")
+text28 <- read.csv(file="Text28_log.csv", head=TRUE,sep=",")
+img_pts <- read.csv(file="Imagept28_log.csv", head=TRUE,sep=",")
+
+#####################   call Sort       ####################################
+getData = function(){
+  sort("Text28_log.csv", "TextScheme.csv")
+  sort("Blankpt28_log.csv", "BlankScheme.csv")
+  sort("Imagept28_log.csv", "ImageScheme.csv")
+}
+
+
+#####################    Sort Function       ####################################
+sort = function(inputFile, outputFile){
+  data <- text28 <- read.csv(file=inputFile, head=TRUE,sep=",")
+  df <- data.frame( user = data$user, scheme = data$scheme, mode = data$mode,  event = data$event,  time = data$time)
+  info <- data.frame(user = character(0), scheme = character(0), event = character(0),  timeTaken_sec = numeric())
+  initTime = 0
+  finalTime = 0
+  e = "NA"
+  s = "NA"
+  
+  for (u in unique(df$user)){ # for user
+    user = u  # set user
+    set <- subset(df, df$user == u )
+ 
+    for(row in 1:nrow(set)){ # for each entry
+      s = set[row,]$scheme # set scheme
+      #create new password
+      if(set[row,]$mode == "create" & set[row,]$event == "start")
+      {
+        e = "Created Password"
+        initTime = set[row,]$time # get initial time
+        
+      }
+      # successful login
+      else if(set[row,]$mode == "login" & set[row,]$event == "success")
+      {
+        e = "Successful Login"
+        
+      }
+      # failed login
+      else if (set[row,]$mode == "login" & set[row,]$event == "failure" )
+      {
+        e = "Failed Login"
+      }else
+      {
+        e   = "testing"
+      }
+      
+      
+      finalTime =  set[row,]$time
+      # get time
+      totalTime <- difftime(strptime(finalTime, format="%Y-%m-%d %H:%M:%S", tz=""), 
+                            strptime(initTime, format="%Y-%m-%d %H:%M:%S", tz=""), units="secs")
+      t =  round(as.numeric(totalTime, units = "secs"), digits = 2)
+      
+      temp = data.frame(user = u, scheme = s, event = e,  timeTaken_sec = t)
+      info <- rbind(info, temp)
+      
+    }
+    
+  }
+  print("File Read")
+  write.csv(info, file = outputFile)
+}
